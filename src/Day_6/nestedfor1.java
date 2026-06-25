2f948a5c371e9be12a520dd1c6c2c55bbd4a09a6
keertana <k96677149@gmail.com>
2026-06-25 14:01:14 +0530

    this program to print year month week day routine uing nested for lopp

--- src/Day_6/nestedfor1.java
+++ src/Day_6/nestedfor1.java
@@ -2,11 +2,16 @@ package Day_6;
 
 public class nestedfor1 {
     public static void main(String[] args){
-        for(int i=1;i<=5;i++){
-            for(int j=1;j<=5;j++){
-                System.out.print(j);
+        for(int year=1;year<=12;year++){
+            for(int month=1;month<=30;month++){
+            for(int week=1;week<=4;week++) {
+                for (int day = 1; day <= 6; day++) {
+                    System.out.println("year " + year + " month " + month + " week " + week + " day " + day);
+                    System.out.println("Come to clg,study,go home");
+                }
+
             }
-            System.out.println();
+            }
         }
     }
 }
