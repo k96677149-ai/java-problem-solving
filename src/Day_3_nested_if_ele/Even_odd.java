6fe4f0aac828d7b17718360887fd0ad6ccb75824
keertana <k96677149@gmail.com>
2026-06-25 22:36:27 +0530

    this program to print even or odd number

---
+++ src/Day_3_nested_if_ele/Even_odd.java
@@ -0,0 +1,27 @@
+package Day_3_nested_if_ele;
+import java.util.Scanner;
+public class Even_odd {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number:");
+        int a = scanner.nextInt();
+
+        if(a>0){
+            if(a%2 == 0){
+                System.out.print("positive even");
+            }else{
+                System.out.print("positive odd");
+            }
+        }else if(a<0){
+
+        if(a%2 == 0){
+                System.out.print("Negative even");
+            }else{
+                System.out.print("Negative odd");
+            }
+        }else{
+            System.out.print("Neutral");
+        }
+        scanner.close();
+    }
+}
