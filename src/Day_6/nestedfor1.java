38917fc42b7368527dc603321d8efc17b88d81db
keertana <k96677149@gmail.com>
2026-06-25 14:59:53 +0530

    this program to print triangle  uing nested for lopp

--- src/Day_6/nestedfor1.java
+++ src/Day_6/nestedfor1.java
@@ -2,11 +2,13 @@ package Day_6;
 
 public class nestedfor1 {
     public static void main(String[] args){
-        for(int year=1;year<=12;year++){
-            for(int month=1;month<=30;month++){
-            for(int week=1;week<=4;week++) {
+        int count = 0;
+            for(int month=1;month<=12;month++){
+                count+=month;
+            for(int week=1;week<=4;week++){
                 for (int day = 1; day <= 6; day++) {
-                    System.out.println("year " + year + " month " + month + " week " + week + " day " + day);
+
+                    System.out.println(  " month " + month + " week " + week + " day " + day + ++count);
                     System.out.println("Come to clg,study,go home");
                 }
 
@@ -14,4 +16,4 @@ public class nestedfor1 {
             }
         }
     }
-}
+
