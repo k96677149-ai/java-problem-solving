617111078b71626849e610ebd420e36aa78edd12
keertana <k96677149@gmail.com>
2026-06-25 12:20:26 +0530

    print 111 222 using nested for loop

--- src/Day_6/nestedfor.java
+++ src/Day_6/nestedfor.java
@@ -4,7 +4,7 @@ public class nestedfor {
     public static void main(String[] args){
         for(int i=0;i<=5;i++){
             for(int j=1;j<=3;j++){
-                System.out.print(j);
+                System.out.print(i);
             }
             System.out.println();
         }
