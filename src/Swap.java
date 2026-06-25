4ff19399c7f7c94f7f1574b5d72d085d09d1c693
keertana <k96677149@gmail.com>
2026-06-25 10:45:43 +0530

    this code for swap to integer using xOR

---
+++ src/Swap.java
@@ -0,0 +1,11 @@
+public class Swap {
+    public static void main(String[] args){
+        int x = 5;
+        int y = 10;
+        x = x ^ y;
+        y = x ^ y;
+        x = x ^ y;
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
