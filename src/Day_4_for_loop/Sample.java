21061fc0f4868277951f3c5e9f40077428305dda
keertana <k96677149@gmail.com>
2026-06-26 11:48:23 +0530

    this program to print hello in loop mode using for loop

---
+++ src/Day_4_for_loop/Sample.java
@@ -0,0 +1,10 @@
+package Day_4_for_loop;
+
+public class Sample {
+    public static void main(String[] args){
+        int i;
+        for(i=0;i<5;i++){
+            System.out.println("Hello");
+        }
+    }
+}
