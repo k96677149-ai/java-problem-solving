ec6698fb2408da0306e34effd023267246afe4ca
keertana <k96677149@gmail.com>
2026-06-26 12:15:51 +0530

    this program to sum the only odd integer from user given range

---
+++ src/Day_4_for_loop/SumOdd.java
@@ -0,0 +1,16 @@
+package Day_4_for_loop;
+import java.util.Scanner;
+public class SumOdd{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int sum = 0;
+        for(int i=1;i<=n;i++){
+            if(i%2!=0){
+                sum+=i;
+            }
+        }
+        System.out.println(sum);
+        scanner.close();
+    }
+}
