d55efc2d7da40223ea809976e172de60f5ea7b64
keertana <k96677149@gmail.com>
2026-06-26 12:24:15 +0530

    this program to print 123 123 using nested for loop

---
+++ src/Day_5/Nestedfor.java
@@ -0,0 +1,13 @@
+package Day_5;
+
+public class Nestedfor {
+    public static void main(String[] args){
+        for (int i=0;i<=5;i++){
+            for(int j=1;j<=3;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
+
