b4997e8370173f71fbccdc10f7616e14c0b2c377
keertana <k96677149@gmail.com>
2026-06-26 12:20:26 +0530

    this program to skip loop using continue statement

---
+++ src/Day_4_for_loop/Continue.java
@@ -0,0 +1,12 @@
+package Day_4_for_loop;
+
+public class Continue {
+    public static void main(String[] args){
+        for (int i=0;i<=5;i++){
+            if(i==2){
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
