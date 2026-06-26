36bafc43b74a8904f443a2b928e51d6dcf3ebd46
keertana <k96677149@gmail.com>
2026-06-26 11:51:59 +0530

    this program to print from 1 to 10 using for loop

---
+++ src/Day_4_for_loop/Print_1_n.java
@@ -0,0 +1,10 @@
+package Day_4_for_loop;
+
+public class Print_1_n {
+    public static void main(String[] args){
+        int i;
+        for(i=1;i<=10;i++){
+            System.out.println(i);
+        }
+    }
+}
