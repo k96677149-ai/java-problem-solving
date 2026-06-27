6347a5dd2cc64281210d8b80c0b10464173fc456
keertana <k96677149@gmail.com>
2026-06-27 12:11:28 +0530

    this program to sum of array integer using array

---
+++ src/Day_7/classtask/SumOfArray.java
@@ -0,0 +1,17 @@
+package Day_7.classtask;
+import java.util.Arrays;
+import java.util.Scanner;
+public class SumOfArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        int[] arr = {1,2,3,4,5};
+        for (int i = 0; i <=4; i++){
+            sum = sum + arr[i];
+
+    }
+        System.out.println(Arrays.toString(arr));
+        System.out.println(sum);
+
+    }
+}
