ea7cde3de04070ddbfa655bb5ed8426fcbc5f5f1
keertana <k96677149@gmail.com>
2026-06-27 13:55:38 +0530

    this program to print the array element

---
+++ src/Day_7/classtask/Arrayelements.java
@@ -0,0 +1,10 @@
+package Day_7.classtask;
+
+public class Arrayelements {
+    public static void main(String[] args) {
+        int[] num = {4, 5, 6, 7, 8, 9, 10};
+        for (int i = 0; i < num.length; i++) {
+            System.out.println(num[i]);
+        }
+    }
+}
